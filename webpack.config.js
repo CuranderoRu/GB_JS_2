354f5a32fedfed942625d7e27904f48c490f94e8
Sergei Mono <sergei.ogarkov@sogarkov.ru>
2020-11-24 21:26:21 +0300

    css processing added

--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,15 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.js$/,
-            use: [
-                { loader: 'babel-loader' }
-            ]
-        }]
+                test: /\.js$/,
+                use: [
+                    { loader: 'babel-loader' }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use[{ loader: 'style-loader' }, { loader: 'css-loader' }]
+            }
+        ]
     }
 }
